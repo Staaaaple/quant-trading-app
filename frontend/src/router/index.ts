54e294067bc5271048268c482d386f5e31a3e381
staple <2841755121@qq.com>
2026-06-24 22:24:17 +0800

    feat(frontend-only): 首页默认使用 DemoView

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DemoView from '../views/DemoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +8,7 @@ const router = createRouter({
       path: '/',
       name: 'home',
       meta: { titleKey: 'nav.home', layout: 'demo' },
-      component: HomeView,
+      component: DemoView,
     },
     {
       path: '/demo',
